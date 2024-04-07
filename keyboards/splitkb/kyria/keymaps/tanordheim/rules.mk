3a6aac21da894ab19e5e02cd9bd65f31f2732efb
Trond Nordheim <trond@nordheim.io>
2024-04-07 12:01:53 +0200

    update kyria keymap

--- keyboards/splitkb/kyria/keymaps/tanordheim/rules.mk
+++ keyboards/splitkb/kyria/keymaps/tanordheim/rules.mk
@@ -3,4 +3,5 @@ ENCODER_ENABLE = no        # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 LEADER_ENABLE = no         # Enable the Leader Key feature
 MOUSEKEY_ENABLE = no
+WPM_ENABLE = yes
 CONVERT_TO = liatris
